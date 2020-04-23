8784b62bd98b73c7848d100c79f1414e23029e1f
Luis Jesús Díaz <luis@drconopoima.com>
2020-04-23 02:14:36 +0200

    Add packages Support for xfat

--- drconopoima_ubuntu_setup.sh
+++ drconopoima_ubuntu_setup.sh
@@ -44,7 +44,7 @@ python-openssl virtualbox vagrant virtualbox-ext-pack krita ibus \
 netcat-openbsd snapd libnotify-bin hwinfo tcpdump gawk fonts-opensymbol \
 kubuntu-restricted-extras kubuntu-restricted-addons kubuntu-wallpapers \
 plasma-workspace-wallpapers coreutils apt-file telnet openssh-client \
-openssh-server gpg net-tools"
+openssh-server gpg net-tools exfat-fuse exfat-utils"
 
 DEFAULT_PACKAGES_TO_REMOVE="gstreamer1.0-fluendo-mp3 telnetd"
 
@@ -245,6 +245,8 @@ fi
 if [[ ! -z ${VSCODE+x} ]]; then
     packages_to_install+=('curl coreutils apt-transport-https')
 fi
+
+add-apt-repository universe
 
 apt-get update
 
